248e2c4c3c71e7909d9dd7dbfa40adc621f46c1d
Unknown-Squid <115359789+Unknown-Squid@users.noreply.github.com>
2022-10-08 22:41:56 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <ol id='list'> 
+
+        <li class="animals">Cat</li> 
+
+        <li class="animals">Dog</li> 
+
+        <li class="animals">Elephant</li> 
+
+        <li class="animals">Fish</li> 
+
+        <li class="animals">Gorilla</li> 
+
+        <li class="animals">Monkey</li> 
+
+        <li class="animals">Turtle</li> 
+
+        <li class="animals">Whale</li> 
+
+        <li class="animals">Aligator</li> 
+
+        <li class="animals">Donkey</li> 
+
+        <li class="animals">Horse</li> 
+
+    </ol>
+
+    </div>
+  )
+}
